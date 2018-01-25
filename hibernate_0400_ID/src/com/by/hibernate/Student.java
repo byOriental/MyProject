6fde7640001b7488e7a1d862e7dc108cc59ea909
byOriental <baichangling@126.com>
2018-01-25 15:53:44 +0800

    hibernate_0400_ID

---
+++ hibernate_0400_ID/src/com/by/hibernate/Student.java
@@ -0,0 +1,33 @@
+package com.by.hibernate;
+
+public class Student {
+	private int id;
+	private String name;
+	private int age;
+
+	// private String id;//使用uuid时，字段类型必须为String
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
