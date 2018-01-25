a71d0167b1bf6d5ffbd703663e01a2a2cd79cad6
byOriental <baichangling@126.com>
2018-01-25 15:57:44 +0800

    hibernate_0800_one2one_uni_pk

---
+++ hibernate_0800_one2one_uni_pk/src/com/by/hibernate/Husband.java
@@ -0,0 +1,44 @@
+package com.by.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+public class Husband {
+	private int id;
+	private String name;
+	private Wife wife;
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	// wifeId÷∏œÚ¿‡Wife
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	public Wife getWife() {
+		return wife;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setWife(Wife wife) {
+		this.wife = wife;
+	}
+
+}
