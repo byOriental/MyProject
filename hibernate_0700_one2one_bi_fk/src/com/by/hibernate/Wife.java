ad328908ce512ab9bbdc9a116d30edcf7c10a843
byOriental <baichangling@126.com>
2018-01-25 15:57:02 +0800

    hibernate_0700_one2one_bi_fk

---
+++ hibernate_0700_one2one_bi_fk/src/com/by/hibernate/Wife.java
@@ -0,0 +1,42 @@
+package com.by.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Wife {
+	private int id;
+	private String name;
+	private Husband husband;
+
+	// 只要一对一双向关联，必设mappedBy(mappedBy = "wife").告诉husband在其类中已经有这个属性
+	@OneToOne(mappedBy = "wife")
+	public Husband getHusband() {
+		return husband;
+	}
+
+	public void setHusband(Husband husband) {
+		this.husband = husband;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
